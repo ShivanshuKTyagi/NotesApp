168f0e10c6a11db518abe18030eaec2a654f46cf
SpazeWalker <shivanshutyagi3@gmail.com>
2019-12-04 22:44:12 +0530

    dashboard updated

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ var bodyparser = require('body-parser');
 var cookiePerser = require('cookie')
 var path = require('path');
 var bcrypt = require('bcryptjs')
-
+const dotenv = require('dotenv');
+dotenv.config();
 
 function makeid(length) {
 	var result           = '';
-	var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*)';
 	var charactersLength = characters.length;
 	for ( var i = 0; i < length; i++ ) {
 	   result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -21,13 +22,18 @@ function makeid(length) {
  }
 
 
+ var mongoKey = process.env.KEY;
+
 //initalising Mongodb
 var users
-const uri ="mongodb+srv://dbuser:lgledishu@cluster0-ucofm.azure.mongodb.net/test?retryWrites=true&w=majority";
+var notes
+const uri =mongoKey;
 const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 client.connect(err => {
 	 users = client.db("NotesApp").collection("Users");
+	 notes = client.db("NotesApp").collection("Notes");
 	console.log("DB Initialised")
+
 });
 
 
@@ -51,33 +57,95 @@ app.use(cookieParser())
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 
-
+//-------------------------------------------------------------------------------------------------
 //handeling get requests
 app.get('/',(req,res)=>{
-	res.render('index');
+	if(req.cookies["id"]){	
+		res.redirect('/dash');
+	}else{
+			res.render('index');
+	}
 })
 
 app.get('/signup',(req,res) =>{
 	res.render('signup');
 });
+
+
 app.get('/login',(req,res) =>{
 	res.render('login');
 });
 
-app.get('/dash',(req,res) =>{
-	users.findOne({id: req.cookies["id"]}, async function(err,item){	
-		console.log("active user: "+item["name"]);
-		res.render('dashboard',{user:item["name"]});
-		});
+app.get('/error',(req,res) =>{
+	res.render('error');
+});
+
+app.get('/logout', (req,res)=> {
+	res.clearCookie('id');
+	res.redirect('/');
 });
 
 
+
+//---------------------------------------------------------------------------------------------
+
+
+
 //handeling post requests
+
+app.post('/delete', (req,res)=> {
+	var id_del = req.body["id"];
+	console.log(id_del);
+	notes.deleteOne({id: id_del});
+	console.log(req.body);
+	res.redirect('/');
+});
+
+app.post('/add',(req,res) =>{
+	users.findOne({id: req.cookies["id"]}, async function(err,item){
+		if(err){
+			res.redirect('/error');
+		}	
+		notes.insertOne({
+			id: item["id"],
+			note: req.body["data"]
+		});
+	});
+	res.redirect('dash');
+	
+});
+
+app.get('/dash',(req,res) =>{
+	var dat = [];
+	if(req.cookies["id"]){
+		users.findOne({id: req.cookies["id"]}, async function(err,item){	
+			console.log("active user: "+item["name"]);
+			var cursor = notes.find({id: req.cookies["id"]});
+			while(await cursor.hasNext()) {
+  				const db_item = await cursor.next();
+				if(db_item==null)return;
+				dat.push((db_item));
+			}
+			res.render('dashboard', { user: item["name"], data: dat});
+			});
+		}
+	else{
+		res.redirect('/');
+}
+		
+		
+});
+
 app.post('/signup', (req,res)=> {
 	var info = req.body;
 	bcrypt.genSalt(10, function(err, salt) {
+		if(err){
+			res.redirect('/error');
+		}
 		bcrypt.hash(info["password"], salt, function(err, hash) {
-			console.log(hash);
+			if(err){
+				res.redirect('/error');
+			}
 		users.insertOne({
 			id: makeid(20),
 			name: info["name"],
@@ -86,17 +154,25 @@ app.post('/signup', (req,res)=> {
 		});
 		});
 	});
+	res.clearCookie('id');
 	console.log("Recieved data and User added");
-	res.render('login',{message: "Successfully SignedIn..."});
+	console.log("found the user");
+	res.redirect('/');
 	});
 
 app.post('/login', (req,res)=> {
 	var info = req.body;
 	users.findOne({email: info["email"]}, (err,item)=>{	
+		if(err){
+			res.redirect('/error');
+		}
 		bcrypt.compare(info["password"], item["password"], function(err, result) {
+			if(err){
+				res.redirect('/error');
+			}
 			if(info["email"]==item["email"] && result){
 				res.clearCookie('id');
-				res.cookie('id', item["id"] );
+				res.cookie('id', item["id"], {maxAge:60000});
 				res.redirect('/dash')
 				console.log("found the user");
 			}
@@ -107,6 +183,11 @@ app.post('/login', (req,res)=> {
 	});
 });
 
+app.post('/edit', (req,res)=> {
+	res.redirect('/');
+});
+
+//-------------------------------------------------------------------------------------
 
 //running server
 app.listen(process.env.PORT || 5000);
